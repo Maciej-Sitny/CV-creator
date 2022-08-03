75b864665a6b922bfc7aff6bd8c6ff051a6e4f44
Maciej <maciejsitny@gmail.com>
2022-08-03 23:47:14 +0200

    Deleted previous div's children in return

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -6,10 +6,6 @@ export default function Preview(){
 
         return (
             <div>
-                <h1>{document.getElementById('firstName').value}</h1>
-                <h1>{document.getElementById('lastName').value}</h1>
-                <h2>{document.getElementById('address').value}</h2>
-
             </div>
         )
 }
