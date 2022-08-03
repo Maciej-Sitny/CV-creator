0e99987a1d269676884cdbf4f4aa8eb4abd4821f
Maciej <maciejsitny@gmail.com>
2022-08-03 23:44:34 +0200

    Created state about all info in app, deleted state in GeneralInfo and passed allInfo from App to GeneralInfo (also added handleChange() and changed all mentions of an old state in GeneralInfo)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
-import { Component } from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+import React from 'react'
 import GeneralInfo from './components/GeneralInfo';
 import Preview from './components/Preview';
 
+//30 minut
+
 export default function App() {
+
+      const [allInfo,setAllInfo] = React.useState({
+        generalInfo: {
+          firstName: '',
+          lastName: '',
+          address: '',
+          phoneNumber: '',
+          email: '',
+          description:''
+        },
+        education: [
+          {course: '',
+          school:'',
+          startDate:'',
+          endDate:'',
+          description:''
+          }
+        ],
+        work: [
+          {
+            company:'',
+            position:'',
+            startDate:'',
+            endDate:'',
+            description:'',
+          }
+        ]
+        
+      })
+
       return (
         <div className='main'>
           <div className='editSide'>
-              <GeneralInfo />
+              <GeneralInfo allInfo= {allInfo} setAllInfo={setAllInfo}/>
           </div>
           <div className = 'previewSide'>
               <Preview />
--- src/components/GeneralInfo.js
+++ src/components/GeneralInfo.js
@@ -1,30 +1,23 @@
 import React from 'react';
 
-export default function GeneralInfo() {
+export default function GeneralInfo(props) {
     
 
-        const [info,setInfo]=React.useState({
-            firstName: '',
-            lastName: '',
-            address: '',
-            phoneNumber: '',
-            email: '',
-            }
-        )
-
         function handleChange(event) {
-            setInfo(prevInfo=>{
-
+            props.setAllInfo(prevInfo=>{
+                console.log(props.allInfo)
+                return {...prevInfo, generalInfo:{...prevInfo.generalInfo,[event.target.name]:event.target.value}}
             })
         }
     
         return (
             <div className='generalInfo'>
-                <input type="text" onChange={handleChange} value = {info.firstName} name="firstName" id="firstName" maxLength='35'/>
-                <input type="text" onChange={handleChange} value = {info.lastName} name="lastName" id="lastName" />
-                <input type="text" onChange={handleChange} value = {info.address} name="address" id="address" />
-                <input type="text" onChange={handleChange} value = {info.phoneNumber} name="phoneNumber" id="phoneNumber" />
-                <input type="text" onChange={handleChange} value = {info.email} name="email" id="email" />
+                <input type="text" placeholder='First Name'   onChange={handleChange} value = {props.allInfo.generalInfo.firstName}   name="firstName" id="firstName" maxLength='35'/>
+                <input type="text" placeholder='Last Name'    onChange={handleChange} value = {props.allInfo.generalInfo.lastName}    name="lastName" id="lastName" />
+                <input type="text" placeholder='Address'      onChange={handleChange} value = {props.allInfo.generalInfo.address}     name="address" id="address" />
+                <input type="text" placeholder='Phone Number' onChange={handleChange} value = {props.allInfo.generalInfo.phoneNumber} name="phoneNumber" id="phoneNumber" />
+                <input type="text" placeholder='E-mail'       onChange={handleChange} value = {props.allInfo.generalInfo.email}       name="email" id="email" />
+                <input type="text" placeholder='Description'  onChange={handleChange} value = {props.allInfo.generalInfo.description} name="description" id="description" />
             </div>
         )
     
